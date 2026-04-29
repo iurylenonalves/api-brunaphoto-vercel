1386195effd2d2b3f79b33733d0cff9bc7063e0c
Iury Lenon Alves <iuryalves.uk@gmail.com>
2026-04-29 20:38:41 +0100

    chore(ci): add lint to backend pipeline and stabilize test env variables

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {},
+  },
+];
